ba36a985d2f300e9f1b45fecf5a73a615af2887f
Julian Ciro <ejbarrera0614@gmail.com>
2023-03-22 23:57:21 -0500

    Se realiza cambios en los alias para hacer pruebas en Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import * as path from 'path'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@ui': path.resolve(__dirname, 'src/hobby-bunker-ui'),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+     
+    /* [
       { find: '@', replacement: path.resolve(__dirname, 'src') }, 
-      { find: '@ui', replacement: path.resolve(__dirname, 'src/hobby-bunker-ui') }]
+      { find: '@ui', replacement: path.resolve(__dirname, 'src/hobby-bunker-ui') }
+    ] */
   }
 })
